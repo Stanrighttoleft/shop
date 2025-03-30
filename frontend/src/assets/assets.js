e515484cbd9ad4e0164d8f2ec3a6b2cd2dfcdbea
stan <moon_saga@yahoo.com.tw>
2025-03-30 23:03:15 +0800

    new

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,93 @@
+import p_img1 from './p_img1.webp'
+import p_img2 from './p_img2.webp'
+import p_img3 from './p_img3.webp'
+import p_img4 from './p_img4.webp'
+import p_img5 from './p_img5.webp'
+import p_img6 from './p_img6.webp'
+import p_img7 from './p_img7.webp'
+import p_img8 from './p_img8.webp'
+import p_img9 from './p_img9.webp'
+import p_img10 from './p_img10.webp'
+import p_img11 from './p_img11.webp'
+import p_img12 from './p_img12.webp'
+import p_img13 from './p_img13.jpeg'
+import p_img14 from './p_img14.webp'
+import p_img15 from './p_img15.webp'
+import p_img16 from './p_img16.webp'
+import p_img17 from './p_img17.webp'
+import p_img18 from './p_img18.webp'
+import p_img19 from './p_img19.webp'
+import p_img20 from './p_img20.webp'
+import p_img21 from './p_img21.webp'
+import p_img22 from './p_img22.webp'
+import p_img23 from './p_img23.webp'
+import p_img24 from './p_img24.webp'
+import p_img25 from './p_img25.webp'
+import p_img26 from './p_img26.webp'
+import p_img27 from './p_img27.webp'
+import p_img28 from './p_img28.webp'
+import p_img29 from './p_img29.webp'
+import p_img30 from './p_img30.webp'
+import p_img31 from './p_img31.webp'
+import p_img32 from './p_img32.webp'
+import p_img33 from './p_img33.webp'
+import p_img34 from './p_img34.webp'
+import p_img35 from './p_img35.webp'
+import p_img36 from './p_img36.webp'
+import p_img37 from './p_img37.webp'
+import p_img38 from './p_img38.webp'
+import p_img39 from './p_img39.webp'
+import p_img40 from './p_img40.webp'
+import p_img41 from './p_img41.webp'
+import p_img42 from './p_img42.webp'
+import p_img43 from './p_img43.webp'
+import p_img44 from './p_img44.webp'
+import p_img45 from './p_img45.webp'
+import p_img46 from './p_img46.webp'
+import p_img47 from './p_img47.webp'
+import p_img48 from './p_img48.webp'
+import p_img49 from './p_img49.webp'
+import p_img50 from './p_img50.webp'
+import p_img51 from './p_img51.webp'
+import p_img52 from './p_img52.webp'
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+
+export const assets={
+    logo,
+    search_icon,
+    profile_icon,
+    
+}
+
+
+export const products=[
+    {
+        _id:"aaaaa",
+        name:"food",
+        description:"a food",
+        price:100,
+        image:[p_img1],
+        category:"soup",
+        subCategory:"vegetable",
+        sizes:["S","M","L"],
+        date:19990505,
+        bestseller:true,
+
+    },
+    {
+        _id:"aaaab",
+        name:"food",
+        description:"a food",
+        price:100,
+        image:[p_img2],
+        category:"soup",
+        subCategory:"vegetable",
+        sizes:["S","M","L"],
+        date:19990505,
+        bestseller:true,
+
+    },
+   
+]
