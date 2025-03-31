28bacb1280c1bdc48b4520479c0db1d71c29186b
stan <moon_saga@yahoo.com.tw>
2025-03-31 21:17:27 +0800

    navbar tags

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -53,11 +53,15 @@ import p_img52 from './p_img52.webp'
 import logo from './logo.png'
 import search_icon from './search_icon.png'
 import profile_icon from './profile_icon.png'
+import cart_icon from './cart_icon.png'
+import menu_icon from './menu_icon.png'
 
 export const assets={
     logo,
     search_icon,
     profile_icon,
+    cart_icon,
+    menu_icon,
     
 }
 
