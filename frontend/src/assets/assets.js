47facfe9f715b4bd55ee0a6a7655b99634b9e107
stan <moon_saga@yahoo.com.tw>
2025-03-31 22:08:22 +0800

    navbar complete

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -55,6 +55,7 @@ import search_icon from './search_icon.png'
 import profile_icon from './profile_icon.png'
 import cart_icon from './cart_icon.png'
 import menu_icon from './menu_icon.png'
+import dropdown_icon from './dropdown_icon.png'
 
 export const assets={
     logo,
@@ -62,6 +63,7 @@ export const assets={
     profile_icon,
     cart_icon,
     menu_icon,
+    dropdown_icon,
     
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -53,9 +53,13 @@ const Navbar = () => {
         <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' :'w-0'}`}>
           <div className="flex flex-col text-gray-600">
             <div onClick={()=>setVisible(false)} className="flex item-center gap-4 p-4 cursor-pointer">
-              <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+              <img className='h-5 mt-0.5 ' src={assets.dropdown_icon} alt="" />
               <p>Back</p>
             </div>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>Home</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
           </div>
         </div>
         
