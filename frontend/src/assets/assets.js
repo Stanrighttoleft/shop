2f3b6272679147b743de10233952736ffa34261e
stan <moon_saga@yahoo.com.tw>
2025-04-07 23:05:33 +0800

    add delete and quantity adjust function to cart page

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -67,6 +67,7 @@ import quality_icon from './quality_icon.png'
 import cross_icon from './cross_icon.png'
 import star_icon from './star_icon.png'
 import star_dull_icon from './star_dull_icon.png'
+import bin_icon from './bin_icon.png'
 
 
 export const assets={
@@ -83,6 +84,7 @@ export const assets={
     cross_icon,
     star_icon, 
     star_dull_icon,
+    bin_icon,
 }
 
 
