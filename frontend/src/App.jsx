83bd5fce394881aca1f0d8a2fa6ed6966382ab9b
stan <moon_saga@yahoo.com.tw>
2025-04-12 21:45:25 +0800

    add another empty route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,7 @@ const App = () => {
       
       
       <Routes>
+        <Route path='' element={<Home/>}/>
         <Route path='/shop/frontend/dist/index.html' element={<Home/>}/>
         <Route path='/' element={<Home/>}/>
         <Route path='/collection' element={<Collection/>}/>
