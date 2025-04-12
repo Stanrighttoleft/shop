9717902a03930ee3976d01c778c4538736c2d284
stan <moon_saga@yahoo.com.tw>
2025-04-12 20:26:54 +0800

    try deploy by modify the package.json homepage and vite config base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base:'/shop',
 })
