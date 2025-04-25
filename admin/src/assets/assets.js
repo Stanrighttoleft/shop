fe44a32b81032e7d470fce349fb862a23f9da40d
stan <moon_saga@yahoo.com.tw>
2025-04-25 22:13:54 +0800

    setup login page for use but not set the login function

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,9 +1,13 @@
 import logo from './logo.png'
 import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import list_icon from './list_icon.png'
 
 
 export const assets={
     logo,
     add_icon,
+    order_icon,
+    list_icon,
 
 }
