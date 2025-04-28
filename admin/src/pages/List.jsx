e72390bc089832b5f3d4b99f885a92ba25f9716a
stan <moon_saga@yahoo.com.tw>
2025-04-28 22:47:22 +0800

    make table for the list item page

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { backendUrl } from '../App'
+import { backendUrl, currency } from '../App'
 import axios from 'axios'
 
 const List = () => {
@@ -7,19 +7,48 @@ const List = () => {
   const fetchList=async()=>{
     try {
       const response =await axios.get(backendUrl+'/api/product/list')
-      console.log(response.data);
-      
-    } catch (error) {
-      
-    }
+      if(response.data.success){
+        setList(response.data.products);
+      }else{
+        toast.error(response.data.message)
+      }
 
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(()=>{
     fetchList()
   },[])
   return (
-    <div>List</div>
+    <>
+    <p className='mb-2'>All Products List</p>
+    <div className='flex flex-col gap-2' >
+      {/**List table title */}
+      <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm' >
+        <b>Image</b>
+        <b>Name</b>
+        <b>Category</b>
+        <b>Price</b>
+        <b className='text-center' >Action</b>
+      </div>
+      {/**Product List */}
+      {
+        list.map((item,index)=>(
+          <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+            <img className='w-12 h-12 overflow-hidden object-contain' src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category} </p>
+            <p>{currency}{item.price} </p>
+            <p className='text-right md:text-center cursor-pointer text-lg' >X</p>
+          </div>
+        ))
+      }
+
+
+    </div>
+    </>
   )
 }
 
