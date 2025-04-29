321802da46360df2e90aef252b429206c6fc577d
stan <moon_saga@yahoo.com.tw>
2025-04-29 22:32:40 +0800

    after login will not enter login page again by refresh page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -15,6 +15,7 @@ const ShopContextProvider=(props)=>{
     const [showSearch,setShowSearch]=useState(false);
     const [cartItems, setCartItems]=useState({});
     const [products,setProducts]=useState([]);
+    const [token, setToken]=useState('');
     const navigate=useNavigate ();
     
     const addToCart=async(itemId,size)=>{
@@ -99,12 +100,17 @@ const ShopContextProvider=(props)=>{
 
     },[])
 
+    useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'))
 
- 
+        }
+    })
+
 
     const value = { 
 
-        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems, addToCart,getCartCount,updateQuantity,getCartAmount,navigate,backendUrl,
+        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems, addToCart,getCartCount,updateQuantity,getCartAmount,navigate,backendUrl, token, setToken,
 
     }
     return(
