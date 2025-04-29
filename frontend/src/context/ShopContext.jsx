e833db650fc6998c8ba256c3b2210b51f5651bd2
stan <moon_saga@yahoo.com.tw>
2025-04-29 20:45:09 +0800

    connect the backend to the frontend by axios but only use console log

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,8 @@
 import { createContext, useEffect, useState } from "react";
-import {products} from "../assets/assets"
+
 import { toast } from "react-toastify";
 import {useNavigate} from "react-router-dom"
+import axios from "axios"
 
 export const ShopContext=createContext();
 
@@ -9,9 +10,11 @@ const ShopContextProvider=(props)=>{
 
     const currency='$';
     const delivery_fee=10;
+    const backendUrl=import.meta.env.VITE_BACKEND_URL
     const [search,setSearch]=useState('');
     const [showSearch,setShowSearch]=useState(false);
     const [cartItems, setCartItems]=useState({});
+    const [products,setProducts]=useState([]);
     const navigate=useNavigate ();
     
     const addToCart=async(itemId,size)=>{
@@ -74,11 +77,29 @@ const ShopContextProvider=(props)=>{
         }
         return totalAmount;
     }
+
+    const getProductsData=async()=>{
+        try {
+            const response = await axios.get(backendUrl+'/api/product/list') 
+            console.log(response.data);
+            console.log("ENV BACKEND URL:", import.meta.env.VITE_BACKEND_URL);
+            
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(()=>{
+        getProductsData()
+
+    },[])
+
+
  
 
     const value = { 
 
-        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems, addToCart,getCartCount,updateQuantity,getCartAmount,navigate
+        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems, addToCart,getCartCount,updateQuantity,getCartAmount,navigate,backendUrl,
 
     }
     return(
