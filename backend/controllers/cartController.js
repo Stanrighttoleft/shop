13ce676f6e5f574a98fe6ebea281372ce7c32651
stan <moon_saga@yahoo.com.tw>
2025-04-30 10:50:52 +0800

    setup route and middleware for cartController

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,20 @@
+
+
+
+//add products to user cart
+
+const addToCart=async()=>{
+    
+}
+//update user cart
+
+const updateCart=async()=>{
+
+}
+//get user cart data
+
+const getUserCart=async()=>{
+
+}
+
+export {addToCart, updateCart, getUserCart}
