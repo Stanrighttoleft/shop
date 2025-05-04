89e03f0fe6d75f4f8e0238d8a83c240cb36526c4
stan <moon_saga@yahoo.com.tw>
2025-05-04 23:40:37 +0800

    make order page display users order

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,9 +1,45 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Orders = () => {
-  const {products,currency}=useContext(ShopContext);
+  const {backendUrl,token,currency}=useContext(ShopContext);
+  const [orderData,setorderData]=useState([])
+  const loadOrderData=async()=>{
+    try {
+      if(!token){
+        
+        return null
+      }
+
+      const response=await axios.post(backendUrl+'/api/order/userorders',{},{headers:{token}})
+      if(response.data.success){
+        let allOrdersItem=[]
+        response.data.orders.map((order)=>{
+          order.items.map((item)=>{
+            item['status']=order.status
+            item['payment']=order.payment
+            item['paymentMethod']=order.paymentMethod
+            item['date']=order.date
+            allOrdersItem.push(item)
+          })
+        })
+        setorderData(allOrdersItem.reverse())
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast(error.message)
+    }
+  }
+
+  useEffect(()=>{
+    loadOrderData()
+  },[token])
+
+
   return (
     <div className='border-t pt-16'>
       <div className="text-2xl">
@@ -11,7 +47,7 @@ const Orders = () => {
       </div>
       <div>
         {
-          products.slice(1,4).map((item,index)=>(
+          orderData.map((item,index)=>(
             <div key={index} className='py-4 border-b text-gray-700 flex flex-clo md:flex-row md:items-center md:justify-between gap-4'>
               <div className="flex items-start gap-6 text-sm">
                 <img className='w-16 sm:w-20 h-20 object-contain' src={item.image[0]} alt="" />
